15c5f62d86bdd8f210cd400eebab0160ef61a4c4
vishalmisharaa <vishal.mishra9470@gmail.com>
2020-06-22 08:08:57 +0530

    adding content type

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html")
 	fmt.Fprint(w, "<h1>this is my awesome website , hello vishal</h1>")
 }
 
