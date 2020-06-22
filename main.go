a07cf55c4e33d5f9630d25be72e1fa66eb57fdfd
vishalmisharaa <vishal.mishra9470@gmail.com>
2020-06-22 12:47:25 +0530

    addded a conytact page

--- main.go
+++ main.go
@@ -7,7 +7,12 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html")
-	fmt.Fprint(w, "<h1>this is my awesome website , hello vishal</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>this is my awesome website , hello vishal</h1>")
+	} else if r.URL.Path == "/contacts" {
+		fmt.Fprint(w, "To get in touch please send an email to <a href=\"mailto:vishalmishraa@gmail.com\">vsihal mishra</a>.")
+	}
+
 }
 
 func main() {
